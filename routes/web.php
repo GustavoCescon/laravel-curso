0b7c40b853e79280fa877f9c93d06694016ee0ba
Gustavo Estanislau Cescon de Oliveira <gustavo.oliveira@cachoeiro.es.gov.br>
2023-01-19 11:55:38 -0300

    Criação de controller de teste, rotas e sintaxe blade

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,31 @@ Route::get('/contato', function () {
 
  */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/sobrenos', [\App\Http\Controllers\SobreController::class, 'sobreNos']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/sobrenos', [\App\Http\Controllers\SobreController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/login', function(){ return 'login';});
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){ return 'clientes';});
-    Route::get('/fornecedor', function(){ return 'fornecedor';});
-    Route::get('/produtos', function(){ return 'produtos';});
+    Route::get('/clientes', function(){ return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedor', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produtos', function(){ return 'produtos';})->name('app.produtos');
 });
+
+
+/* Route::get('/rota1', function(){
+    echo 'ROTA 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2'); 
+
+Route::redirect('/rota2', '/rota1');
+*/
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'" >clique aqui</a> para ir para a página inicial';
+});
+
